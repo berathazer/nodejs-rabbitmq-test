6300e64174c58d240ac8d5bf753167c6310bb465
Berat Hazer <berathazer371@gmail.com>
2023-12-08 00:25:08 +0300

    argümana göre kuyruk oluşturulup veri aktarımı yapıldı

--- consumer.js
+++ consumer.js
@@ -1,19 +1,19 @@
 import amqp from "amqplib";
 
+const queue = process.argv[2] || "jobsQueue";
+
 async function connect_rabbitMQ() {
 	try {
 		const connection = await amqp.connect("amqp://localhost:5672");
 
 		const channel = await connection.createChannel();
 
-		const queue = "jobsQueue";
-
 		const assertion = await channel.assertQueue(queue);
 
 		// mesajın alınması
-
+		console.log("Mesajlar Bekleniyor...");
 		channel.consume(queue, (message) => {
-			console.log("Message:", message.content.toString());
+			console.log("Alınan Mesaj:", message.content.toString());
 			channel.ack(message); //gelen mesajın işlendiğini bildiriyoruz bu şekilde mesaj kuyruktan siliniyor.
 		});
 	} catch (error) {
--- publisher.js
+++ publisher.js
@@ -4,19 +4,21 @@ const message = {
 	description: "Test mesajı.",
 };
 
+const queue = process.argv[2] || "jobsQueue";
+
 async function connect_rabbitMQ() {
 	try {
 		const connection = await amqp.connect("amqp://localhost:5672");
 
 		const channel = await connection.createChannel();
 
-		const queue = "jobsQueue";
+		await channel.assertQueue(queue);
 
-		const assertion = await channel.assertQueue(queue);
-
-		channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-
-		console.log("Gönderilen mesaj:", message);
+		setInterval(() => {
+			message.description = new Date().getTime();
+			channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+			console.log("Gönderilen mesaj:", message);
+		}, 10);
 	} catch (error) {
 		console.log("Error:", error);
 	}
